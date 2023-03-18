8cee89cc9fd76785815fe1db764285fdb98cb300
Danny Diaz <danny.diaz@utexas.edu>
2023-03-18 17:51:03 -0500

    formatting changes

--- MutComputeX/data_loader.py
+++ MutComputeX/data_loader.py
@@ -5,11 +5,9 @@ import pickle as pkl
 import numpy as np
 
 
-
-
-def load_dataset(pkl_file: Path) -> Tuple[List[Dict[str,Union[str,int]]], np.ndarray]:
+def load_dataset(pkl_file: Path) -> Tuple[List[Dict[str, Union[str, int]]], np.ndarray]:
     """Snapshots provide the residue order of the boxes"""
-    
+
     assert isinstance(pkl_file, Path)
     assert pkl_file.is_file(), pkl_file.resolve()
     assert pkl_file.suffix == ".pkl", pkl_file.suffix
