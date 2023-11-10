7b08182ad2235d41b3084869fe8cbf1a2bd51c70
Danny Diaz <danny.jesus.diaz.94@gmail.com>
2023-11-10 00:05:48 -0600

    refactored inference code; added capability to run on cpu from the cli.

--- scripts/generate_norbelladine_predictions.py
+++ scripts/generate_norbelladine_predictions.py
@@ -2,8 +2,44 @@ from pathlib import Path
 from typing import List
 from os import environ
 from argparse import ArgumentParser
+from pprint import pprint
 
 from MutComputeX.inference import EnsembleCIFPredictor
+
+
+def cli():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--model-dir",
+        default=Path("../models"),
+        type=Path,
+        help="directory where model checkpoints are located",
+    )
+
+    parser.add_argument(
+        "--model-glob",
+        type=str,
+        default="*",
+        help="glob to select specific models/directories in the model-dir folder",
+    )
+    parser.add_argument(
+        "--data",
+        default=Path("../data/norbelladine_4OMTase/boxes/4OMTase_dataset.pkl"),
+        type=Path,
+        help="Pickle file with serialized microenvironments and snapshots",
+    )
+    parser.add_argument("--out-file", default=None, type=Path)
+    parser.add_argument("--use-cpu", action="store_true")
+
+    args = parser.parse_args()
+
+    assert args.model_dir.is_dir()
+    assert args.data.is_file()
+    assert (
+        args.data.suffix == ".pkl"
+    ), f"{args.data.resolve()} must be a pickle file (.pkl)"
+
+    return args
 
 
 def generate_inference(models: List[Path], data: Path, out_file: Path = None):
@@ -21,20 +57,24 @@ def generate_inference(models: List[Path], data: Path, out_file: Path = None):
 
 
 if __name__ == "__main__":
-    environ["TF_FORCE_GPU_ALLOW_GROWTH"] = "true"
-    environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+    args = cli()
 
-    parser = ArgumentParser()
-    parser.add_argument(
-        "--data",
-        default=Path("../data/norbelladine_4OMTase/boxes/4OMTase_dataset.pkl"),
-        type=Path,
-    )
-    parser.add_argument("--out-file", default=None)
-    args = vars(parser.parse_args())
+    if args.use_cpu:
+        environ["CUDA_VISIBLE_DEVICES"] = ""
 
-    model_dir = Path("../models/")
+    else:
+        environ["TF_FORCE_GPU_ALLOW_GROWTH"] = "true"
+        environ["TF_CPP_MIN_LOG_LEVEL"] = "5"
 
-    models = [m_dir for m_dir in model_dir.iterdir() if m_dir.is_dir()]
+    models = [
+        m_dir.resolve()
+        for m_dir in args.model_dir.glob(args.model_glob)
+        if m_dir.is_dir()
+    ]
 
-    generate_inference(models, **args)
+    print(f"cli options:")
+    pprint(vars(args))
+    print(f"\nSelected model directories:")
+    pprint(models)
+
+    generate_inference(models, args.data, args.out_file)
